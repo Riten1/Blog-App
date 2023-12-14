19859cc7b8d9bed7db1e616d5152ac643f6eda04
Riten1 <122423532+Riten1@users.noreply.github.com>
2023-12-14 16:30:31 +0545

    'formatted'

--- blogapp/src/components/Header/Button.jsx
+++ blogapp/src/components/Header/Button.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const Button = ({children, type = 'button', bgColor = 'bg-blue-600', textColor = 'text-white', className = '', ...props}) => {
+const Button = ({
+  children,
+  type = "button",
+  bgColor = "bg-blue-600",
+  textColor = "text-white",
+  className = "",
+  ...props
+}) => {
   return (
-    <button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor}`} {...props}>{children}</button>
-  )
-}
+    <button
+      className={`px-4 py-2 rounded-lg ${bgColor} ${textColor}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
--- blogapp/src/components/Header/LogoutBtn.jsx
+++ blogapp/src/components/Header/LogoutBtn.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import auth from '../../appwrite/auth'
-import  { logout } from '../../store/authSlice'
-import useDispatch from 'react-redux'
+import React from "react";
+import auth from "../../appwrite/auth";
+import { logout } from "../../store/authSlice";
+import useDispatch from "react-redux";
 
 const LogoutBtn = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  function handleLogout (){
+  function handleLogout() {
     auth.logout().then(() => {
-      dispatch(logout())
-    })
+      dispatch(logout());
+    });
   }
 
   return (
-   <button className='inline-block px-6 py-2 duration-200 hover: bg-blue-100 rounded-full' onClick={handleLogout}>Logout</button>
-  )
-}
+    <button
+      className="inline-block px-6 py-2 duration-200 hover: bg-blue-100 rounded-full"
+      onClick={handleLogout}
+    >
+      Logout
+    </button>
+  );
+};
 
-export default LogoutBtn
+export default LogoutBtn;
